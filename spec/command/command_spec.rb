346e06196914ab0a02c51a77b5826dc3910084f5
Max Meyer <dev@fedux.org>
2012-08-14 16:25:19 +0200

    new syntax of rspec

--- spec/command/command_spec.rb
+++ spec/command/command_spec.rb
@@ -41,6 +41,9 @@ describe Command do
   it "checks if exec is executable" do
     command = Command.new('/bin/true')
     expect(command.executable?).to eq(true)
+
+    command = Command.new('/etc/passwd')
+    expect(command.executable?).to eq(false)
   end
 
   it "checks if exec exists" do
@@ -66,47 +69,48 @@ describe Command do
   end
 
   it "has parameter" do
-    command.parameter.should == 'hello world'
+    expect(command.parameter).to eq('hello world')
   end
 
   it "has options" do
-    command.options.should == ''
+    expect(command.options).to eq('')
   end
 
-  it "offers the possibility to change the working directory of the process" do
-    command.working_directory.should == '/tmp'
+  it "offers the possibility to change the working directory of the process without any side effects" do
+    expect(command.working_directory).to eq('/tmp')
 
-    Dir.pwd.should == File.expand_path('../..', File.dirname(__FILE__))
+    #no side effects
     lambda { command.run }
-    Dir.pwd.should == File.expand_path('../..', File.dirname(__FILE__))
+
+    expect(Dir.pwd).to eq(File.expand_path('../..', File.dirname(__FILE__)))
   end
 
   it "has special keywords indicating errors in stdout" do
-    command.error_keywords.should == %q[abc def]
+    expect(command.error_keywords).to eq(%q[abc def])
   end
 
   it "can be used to construct a command string, which can be executed" do
     command = Command.new(:pdflatex, :log_level => :silent, :logger => logger, :parameter => "index.tex blub.tex", :options => "-a -b")
-    command.send(:build_cmd_string).should == "/usr/bin/pdflatex -a -b index.tex blub.tex"
+    expect(command.send(:build_cmd_string)).to eq("/usr/bin/pdflatex -a -b index.tex blub.tex")
   end
 
   it "runs programms" do
     command.run
-    command.result.should == true
+    expect(command.result).to eq(true)
   end
 
   it "returns the textual rep of a command" do
-    command.to_txt.should == '/bin/echo hello world'
+    expect(command.to_txt).to eq('/bin/echo hello world')
   end
 
   it "execute existing programs" do
     command = Command.execute(:echo, :log_level => :silent, :logger => logger ,:parameter => "index.tex blub.tex", :options => "-- -a -b")
-    command.result.should == true
+    expect(command.result).to eq(true)
   end
   
   it "does not execute non-existing programs" do
     command = Command.execute(:grep, :log_level => :silent, :logger => logger, :parameter => "index.tex blub.tex", :options => "-- -a -b")
-    command.result.should == false
+    expect(command.result).to eq(false)
   end
 
   it "checks if errors have happend during execution" do
@@ -126,30 +130,41 @@ describe Command do
 
     #result = command.send(:help_logger)({ :error_in_exec => true , :error_in_stdout => false} , { :log_file => log_file, :stderr => stderr , :stdout => stdout })
     result = command.send(:help_output, { :error_in_exec => true , :error_in_stdout => false} , { :log_file => log_file, :stderr => stderr , :stdout => stdout })
-    result.should == ["================== LOGFILE ================== ", "Error in ... found", "================== STDOUT ================== ", "Error found", "================== STDERR ================== ", "Error found"]
+    expect(result).to eq( ["================== LOGFILE ================== ", 
+                          "Error in ... found", 
+                          "================== STDOUT ================== ",
+                          "Error found", 
+                          "================== STDERR ================== ", 
+                          "Error found"] )
 
     result = command.send(:help_output, { :error_in_exec => false , :error_in_stdout => true} , { :log_file => log_file, :stderr => stderr , :stdout => stdout })
-    result.should == ["================== STDOUT ================== ", "Error found"]
+    expect(result).to eq(["================== STDOUT ================== ", 
+                          "Error found"] )
 
     result = command.send(:help_output, { :error_in_exec => true , :error_in_stdout => true} , { :log_file => log_file, :stderr => stderr , :stdout => stdout })
-    result.should == ["================== LOGFILE ================== ", "Error in ... found", "================== STDOUT ================== ", "Error found", "================== STDERR ================== ", "Error found"]
+    expect(result).to eq(["================== LOGFILE ================== ",
+                      "Error in ... found",
+                      "================== STDOUT ================== ",
+                      "Error found",
+                      "================== STDERR ================== ",
+                      "Error found"])
 
 
     result = command.send(:help_output, { :error_in_exec => false , :error_in_stdout => false} , { :log_file => log_file, :stderr => stderr , :stdout => stdout })
-    result.should == []
+    expect(result).to eq([])
 
   end
 
   it "finds errors in stdout" do
-    command.send(:error_in_string_found?, ['error'] , 'long string witherror inside' ).should == true
-    command.send(:error_in_string_found?, ['long', 'inside'] , 'long string witherror inside' ).should == true
-    command.send(:error_in_string_found?, ['error'] , 'long string with erro inside' ).should == false
+    expect(command.send(:error_in_string_found?, ['error'] , 'long string witherror inside' )).to eq(true)
+    expect(command.send(:error_in_string_found?, ['long', 'inside'] , 'long string witherror inside' )).to eq(true)
+    expect(command.send(:error_in_string_found?, ['error'] , 'long string with erro inside' )).to eq(false)
   end
 
   it "output a message" do
-    command.send(:message, false, 'Hello_world').should == "\e[1m\e[31mFAILED\e[0m\e[0m\nHello_world"
-    command.send(:message, true, 'Hello_world').should == "\e[1m\e[32mOK\e[0m\e[0m"
-    command.send(:message, true ).should == "\e[1m\e[32mOK\e[0m\e[0m"
+    expect(command.send(:message, false, 'Hello_world')).to eq( "\e[1m\e[31mFAILED\e[0m\e[0m\nHello_world" )
+    expect(command.send(:message, true, 'Hello_world')).to eq("\e[1m\e[32mOK\e[0m\e[0m")
+    expect(command.send(:message, true )).to eq("\e[1m\e[32mOK\e[0m\e[0m")
   end
 
   it "is very verbose and returns a lot of output" do
@@ -157,7 +172,7 @@ describe Command do
     logger = Logger.new(bucket)
     Command.execute(:echo, :logger => logger ,:parameter => "index.tex blub.tex", :options => "-- -a -b" , :log_level => :debug)
 
-    bucket.string.should =~ /OK/
+    expect(bucket.string['OK']).to eq('OK')
   end
 
   it "is silent and returns no output" do
@@ -165,7 +180,7 @@ describe Command do
     logger = Logger.new(bucket)
     Command.execute(:echo, :logger => logger ,:parameter => "index.tex blub.tex", :options => "-- -a -b" , :log_level => :silent)
 
-    bucket.string.should == ""
+    expect(bucket.string).to eq("")
   end
 
   # not completed
@@ -181,6 +196,4 @@ describe Command do
 
   end
   
-  it "resolves path name" do
-  end
 end
