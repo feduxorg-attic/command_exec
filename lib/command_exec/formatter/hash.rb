89ede03be61daa4820a27b9d6393c9b3e8653e73
Max Meyer <dev@fedux.org>
2012-08-30 08:32:19 +0200

    restructured libs

--- lib/command_exec/formatter/hash.rb
+++ lib/command_exec/formatter/hash.rb
@@ -2,21 +2,46 @@
 
 module CommandExec
   module Formatter
-    class Hash < Base
+    class Hash
 
       attr_reader :output
       attr_writer :logger
 
-      public
+    public
 
       def initialize(options={})
-        super
-
         @options = {
           logger: Logger.new($stdout),
         }.deep_merge options
 
         @logger = @options[:logger]
+
+        @log_file = []
+        @return_code = []
+        @stderr = []
+        @stdout = []
+        @status = []
+        @reason_for_failure = []
+      end
+
+      def log_file(*content)
+        @log_file += content.flatten
+      end
+
+      def return_code(*content)
+        @return_code += content.flatten
+      end
+
+      def stdout(*content)
+        @stdout += content.flatten
+      end
+
+      def stderr(*content)
+        @stderr += content.flatten
+      end
+
+      def reason_for_failure(*content)
+        @reason_for_failure += content.flatten
       end
 
       def status(value)
@@ -32,7 +57,9 @@ module CommandExec
         @status
       end
 
-      def output(*fields)
+    private
+
+      def prepare_output(fields=[])
         out = {}
 
         avail_fields = {
@@ -45,11 +72,17 @@ module CommandExec
         }
         fields = [:status,:return_code,:stderr,:stdout,:log_file,:reason_for_failure] if fields.blank?
 
-        fields.flatten.each do |f|
+        fields.each do |f|
           out[f] = avail_fields[f] if avail_fields.has_key?(f)
         end
 
         out
+      end
+
+    public
+
+      def output(*fields)
+        prepare_output fields.flatten
       end
     end
   end
--- lib/command_exec/formatter/json.rb
+++ lib/command_exec/formatter/json.rb
@@ -3,34 +3,8 @@
 module CommandExec
   module Formatter
     class JSON < Hash
-
-      attr_reader :output
-      attr_writer :logger
-
-      public
-
-      def initialize(options={})
-        super 
-      end
-
       def output(*fields)
-        out = {}
-
-        avail_fields = {
-          :status => @status,
-          :return_code => @return_code,
-          :stderr => @stderr,
-          :stdout => @stdout,
-          :log_file => @log_file,
-          :reason_for_failure => @reason_for_failure,
-        }
-        fields = [:status,:return_code,:stderr,:stdout,:log_file,:reason_for_failure] if fields.blank?
-
-        fields.flatten.each do |f|
-          out[f] = avail_fields[f] if avail_fields.has_key?(f)
-        end
-
-        ::JSON.generate out
+        ::JSON.generate prepare_output(fields.flatten) 
       end
     end
   end
