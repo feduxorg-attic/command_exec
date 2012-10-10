5d7fa9d4b0c50744431827e42e1b1ee52023e7d6
Max Meyer <dev@fedux.org>
2012-10-10 16:51:04 +0200

    add documentation to the formatting classes

--- lib/command_exec/formatter/hash.rb
+++ lib/command_exec/formatter/hash.rb
@@ -4,11 +4,23 @@ module CommandExec
   module Formatter
     class Hash
 
+      # @!attribute [r] output
+      #   return the formatted output
       attr_reader :output
+      # @!attribute [w] logger
+      #   set the logger after object creation
       attr_writer :logger
 
-    public
+      public
 
+      # Create new hash formatter
+      # 
+      # @param [Hash] options
+      #   Options for formatter
+      #
+      # @option options [Symbol] :logger
+      #   Logger to output information. Needs to have the same interface like
+      #   the ruby `Logger`-class.
       def initialize(options={})
         @options = {
           logger: Logger.new($stdout),
@@ -25,30 +37,79 @@ module CommandExec
         @reason_for_failure = []
       end
 
+      # Set the content of the log file
+      #
+      # @param content [Array,String]
+      #   The content of log file
+      #
+      # @return [Array] the content of the log file
       def log_file(*content)
         @log_file += content.flatten
       end
 
-      def return_code(*content)
-        @return_code += content.flatten
+      # Set the return code of the command
+      #
+      # @param value [Number,String]
+      #   Set the return code(s) of the command. 
+      #
+      # @return [Array] the return code
+      def return_code(value)
+        @return_code[0] = value.to_s
+
+        @return_code
       end
 
+      # Set the content of stdout
+      #
+      # @param content [Array,String]
+      #   The content of stdout
+      #
+      # @return [Array]
       def stdout(*content)
         @stdout += content.flatten
       end
 
+      # Set the content of stderr
+      #
+      # @param content [Array,String]
+      #   The content of stderr
+      #
+      # @return [Array]
       def stderr(*content)
         @stderr += content.flatten
       end
 
+      # Set the pid of the command
+      #
+      # @param value [Number,String]
+      #   Set the pid of the command. 
+      #
+      # @return [Array]
       def pid(value)
         @pid[0] = value.to_s
+
+        @pid
       end
 
+      # Set the reason for failure
+      #
+      # @param content [Array, String] 
+      #   Set the reason for failure.
+      #
+      # @return [Array]
       def reason_for_failure(*content)
         @reason_for_failure += content.flatten
       end
 
+      # Set the status of the command
+      #
+      # param [String,Symbol] value
+      #   Set the status of the command based on input.
+      #
+      # @return [Array] 
+      #   the formatted status. It returns `OK` in bold and green if status is
+      #   `:success` and `FAILED` in bold and red if status is `:failed`.
+      #
       def status(value)
         case value.to_s
         when 'success'
@@ -62,8 +123,15 @@ module CommandExec
         @status
       end
 
-    private
+      private
 
+      # Build the data structure for output
+      #
+      # @param [Array] fields
+      #   which fields should be outputted
+      #
+      # @return [Hash] 
+      #   the formatted output
       def prepare_output(fields=[])
         out = {}
 
@@ -86,8 +154,15 @@ module CommandExec
         out
       end
 
-    public
+      public
 
+      # Output the prepared output
+      #
+      # @param [Array,Symbol) fields
+      #   the fields which should be outputted
+      #
+      # @return [Hash] 
+      #   the formatted output
       def output(*fields)
         prepare_output(fields.flatten)
       end
--- lib/command_exec/formatter/string.rb
+++ lib/command_exec/formatter/string.rb
@@ -1,8 +1,19 @@
 #encoding: utf-8
 
+#Main
 module CommandExec
+  #Formatting output
   module Formatter
+    #Style as simple string
     class String < CommandExec::Formatter::Array
+
+      # convert the prepared output to single string
+      #
+      # @param [Array,Symbol) fields
+      #   the fields which should be outputted
+      #
+      # @return [String] 
+      #   the output formatted as simple string
       def output(*fields)
         prepare_output(fields.flatten).join("\n")
       end
