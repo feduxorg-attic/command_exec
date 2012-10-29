6c487bdad26f70c8de44dbb5b3de3e312fa00a69
Max Meyer <dev@fedux.org>
2012-10-29 15:33:07 +0100

    easier testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,8 @@ include CommandExec::Exceptions
 
 RSpec.configure do |c|
   c.include CommandExec::SpecHelper
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.filter_run_including :focus => true
 end
 
 #ENV['PATH'] = '/bin'
