a492a604ee721821d021b39c0f2b20af7149b6f2
Max Meyer <dev@fedux.org>
2013-12-06 16:52:35 +0100

    Measure test coverage correctly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 #encoding: utf-8
 $LOAD_PATH << File.expand_path('../lib' , File.dirname(__FILE__))
 
+require_relative 'support/code_quality'
+
 Dir.glob( File.expand_path( '../support/*.rb', __FILE__ ) ).each { |f| require f } if Dir.exists? File.expand_path( '../support', __FILE__ )
