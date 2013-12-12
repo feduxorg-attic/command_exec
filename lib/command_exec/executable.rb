1db28e34770f1923438bbc51ab1c71afdc64eb1e
Max Meyer <dev@fedux.org>
2013-12-12 17:20:07 +0100

    Moved a lot to path resolver

--- lib/command_exec/executable.rb
+++ lib/command_exec/executable.rb
@@ -3,61 +3,35 @@ module CommandExec
 
     # @!attribute [r] path
     #   path to executable
-    attr_reader :path
-    private     :path
+    attr_reader :path, :path_resolver, :path_cleaner
+    private     :path, :path_resolver, :path_cleaner
 
     # Create executable
     #
     # @param [ Path ] path
     #   path to executable
-    def initialize( p, options = {} )
-      @path          = p
-      @path_resolver = PathResolver.new( search_paths( p , options ) )
+    def initialize( local_path, options = {} )
+      @path          = local_path
+      @path_resolver = PathResolver.new( search_paths: determine_search_paths( local_path , options ) )
       @path_cleaner  = PathCleaner.new
 
       after_init
     end
 
-    def after_init; end
 
     # Absolute path to executable
     #
     # @return [String] absolute path to executable, '' if lookup failed
     def absolute_path
-      @path_resolver.absolute_path( @path_cleaner.cleanup( path ) )
+      path_resolver.absolute_path( path_cleaner.cleanup( path.to_s ) )
     end
 
     private
 
-    def exists?
-      File.exists? absolute_path
-    end
+    def after_init; end
 
-    def executable?
-      File.executable? absolute_path
-    end
-
-    def file?
-      File.file? absolute_path
-    end
-
-    public
-
-    # Validate executable
-    #
-    # @raise [CommandExec::Exceptions::CommandNotFound] if command does not exist
-    # @raise [CommandExec::Exceptions::CommandNotExecutable] if command is not executable
-    # @raise [CommandExec::Exceptions::CommandIsNotAFile] if command is not a file
-    def validate
-      raise Exceptions::CommandNotFound , "Command '#{path}' not found." unless exists?
-      raise Exceptions::CommandIsNotAFile, "Command '#{path}' not a file." unless file?
-      raise Exceptions::CommandIsNotExecutable , "Command '#{path}' not executable." unless executable?
-    end
-
-    private
-
-    def search_paths( p, options )
-      Array( options.fetch( :search_paths , SearchPath.new( p ).to_a ) )
+    def determine_search_paths( local_path, hash )
+      Array( hash.fetch( :search_paths , SearchPath.new( local_path ).to_a ) )
     end
 
   end
--- spec/executable_spec.rb
+++ spec/executable_spec.rb
@@ -2,93 +2,37 @@ require 'spec_helper'
 
 describe Executable do
 
-  context '#exists?' do
-    it "succeeds if file exists" do
-      file = create_file( 'file', '', 0755 )
-      exec = Executable.new( file )
-      expect( exec ).to be_exists
-    end
-
-    it "fails if file does not exist" do
-      exec = Executable.new( 'asdf' )
-      expect( exec ).not_to be_file
-    end
-
-    it "works with symbols as well" do
-      file = create_file( 'file', '' , 0755 )
-      exec = Executable.new( :file, search_paths: working_directory )
-      expect( exec ).to be_exists
-    end
-
-  end
-
-  context '#file?' do
-    it "succeeds if file exists and really is a file" do
-      exec = Executable.new( '/usr/bin/which' )
-      expect( exec ).to be_file
-    end
-
-    it "fails if file exists but is not a file" do
-      dir = create_directory( 'directory' )
-      exec = Executable.new( dir )
-      expect( exec ).not_to be_file
-    end
- 
-    it "supports symbols as well" do
-      dir = create_file( 'file', '', 0755 )
-      exec = Executable.new( :file, search_paths: working_directory )
-      expect( exec ).to be_file
-    end end
-
-  context '#executable?' do
-    it "succeeds if file is executable" do
-      exec = Executable.new( '/usr/bin/which' )
-      expect( exec ).to be_executable
-    end
-
-    it "fails if file is not executable" do
-      file = create_file( 'blub', '', 0644 )
-      exec = Executable.new( file )
-      expect( exec ).not_to be_executable
-    end
-
-    it "supports symbols as well" do
-      file = create_file( 'blub', '', 0755 )
-      exec = Executable.new( :blub, search_paths: working_directory )
-      expect( exec ).to be_executable
-    end
-  end
-
   context '#absolute_path' do
-    it "returns absolute path to command" do
-      exec = Executable.new( :which )
-      isolated_environment 'PATH' => '/usr/bin' do
-        expect( exec.absolute_path ).to eq( '/usr/bin/which' )
+    it "returns path to command given as symbol" do
+      cmd  = :file
+      file = create_file( cmd.to_s, '', 0755 )
+
+      exec = isolated_environment 'PATH' => working_directory do
+        Executable.new( cmd )
       end
-    end
-  end
 
-  context '#validate' do
-    it "does nothing if all tests pass" do
-      exec = Executable.new( '/usr/bin/which' )
-      expect { exec.validate }.not_to raise_error
+      expect( exec.absolute_path ).to eq( file )
     end
 
-    it "raises an exception if exist-test fails" do
-      exec = Executable.new( 'asdf')
-      expect { exec.validate }.to raise_error CommandExec::Exceptions::CommandNotFound
+    it "returns path to command given as string" do
+      cmd  = 'file'
+      file = create_file( cmd, '', 0755 )
+
+      exec = switch_to_working_directory do
+        Executable.new( cmd )
+      end
+
+      expect( exec.absolute_path ).to eq( file )
     end
 
-    it "raises an exception if file-test fails" do
-      dir = create_directory( 'directory' )
-      exec = Executable.new( dir )
-      expect { exec.validate }.to raise_error CommandExec::Exceptions::CommandIsNotAFile
-    end
+    it "returns path to command given as absolute path as string" do
+      cmd = create_file( 'file', '', 0755 )
 
-    it "raises an exception if executable-test fails" do
-      file = create_file( 'file', '', 0644 )
-      exec = Executable.new( file )
-      expect { exec.validate }.to raise_error CommandExec::Exceptions::CommandNotExecutable
+      exec = switch_to_working_directory do
+        Executable.new( cmd )
+      end
+
+      expect( exec.absolute_path ).to eq( cmd )
     end
   end
 end
