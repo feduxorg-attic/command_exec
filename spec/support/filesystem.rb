01bef31f3c48ca0aa70c1a5edef718e112d684bf
Max Meyer <dev@fedux.org>
2013-12-12 18:07:34 +0100

    Add helper for tests

--- spec/support/filesystem.rb
+++ spec/support/filesystem.rb
@@ -4,3 +4,10 @@ include FeduxOrg::Stdlib::Filesystem
 def root_directory
   CommandExec.root_directory
 end
+
+def in_directory( directory, &block )
+  Dir.chdir( directory ) do
+    block.call( directory)
+  end
+end
+
