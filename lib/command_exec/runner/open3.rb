ffc39e38cb329ee3edbbc891376231cf7c008c0f
Max Meyer <dev@fedux.org>
2013-12-20 16:55:09 +0100

    Cleanup

--- lib/command_exec/runner/open3.rb
+++ lib/command_exec/runner/open3.rb
@@ -1,7 +1,7 @@
+# encoding: utf-8
 module CommandExec
   module Runner
     class Open3
-
       private
 
       # @!attribute [r] logger
@@ -12,27 +12,23 @@ module CommandExec
 
       # Create new runner
       #
-      # @param [#absolute_path] execuctable
-      #   the executable to be run
-      # @param [#to_a] options
-      #   the options for the executable
-      # @param [#to_a] parameters
-      #   the parameters for the executable
-      # @param [String] working_directory
-      #   the working_directory for the executable
       # @param [#debug,#info,#warn,#error] logger
       #   the logger
       def initialize(logger)
         @logger = logger
       end
 
+      # Create new runner
+      #
+      # @param [#to_s,#working_directory] command
+      #   the command to be run
       def run(command)
         process = Process.new(lib_logger: logger)
 
         ::Open3::popen3(command.to_s, chdir: command.working_directory) do |stdin, stdout, stderr, wait_thr|
-          process.stdout = stdout.readlines.map(&:chomp)
-          process.stderr = stderr.readlines.map(&:chomp)
-          process.pid = wait_thr.pid
+          process.stdout      = stdout.readlines.map(&:chomp)
+          process.stderr      = stderr.readlines.map(&:chomp)
+          process.pid         = wait_thr.pid
           process.return_code = wait_thr.value.exitstatus
 
           process
